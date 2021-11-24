b71ad821d1b239f6b0d7f9d22235f9464cb8d5a8
Anes Mulalic <anes.mulalic@outlook.com>
2021-11-24 10:27:22 +0100

    Change WPGraphQL source

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
          // https://limomet.a2hosted.com/websby/graphql
          // http://127.0.0.1/wpgatsby/graphql
 
-         url: `http://127.0.0.1/wpgatsby/graphql`,
+         url: `http://lifeofawinner.com/graphql`,
       },
    },
     `gatsby-plugin-react-helmet`,
